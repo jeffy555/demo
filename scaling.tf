907c3512fe490dd3de3ed6296540ca8f5467bce8
jeffy555 <jeffersonimmanuel5@gmail.com>
2021-05-10 15:29:11 +0530

    Update scaling.tf

--- scaling.tf
+++ scaling.tf
@@ -18,8 +18,18 @@ resource "aws_launch_configuration" "launchme" {
   name          = "web_config"
   image_id      = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
-  user_data = files("userdata.sh")
-  
+  user_data = <<-EOF
+#!/bin/bash
+sudo yum update -y 
+sudo yum install -y httpd24 php72 mysql57-server php72-mysqlnd
+sudo service httpd start 
+sudo chkconfig httpd on 
+sudo usermod -a -G apache ec2-user 
+sudo chown -R ec2-user:apache /var/www 
+sudo chmod 2775 /var/www 
+find /var/www -type d -exec sudo chmod 2775 {} \; find /var/www -type f -exec sudo chmod 0664 {} \; 
+echo “<?php phpinfo(); ?>” > /var/www/html/phpinfo.php
+EOF
 }
 
 resource "aws_placement_group" "test" {
