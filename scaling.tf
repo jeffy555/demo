4ccec838e849f4a0f64b995419a9e49d74727aca
jeffy555 <jeffersonimmanuel5@gmail.com>
2021-05-10 15:39:15 +0530

    Update scaling.tf

--- scaling.tf
+++ scaling.tf
@@ -16,7 +16,7 @@ resource "aws_db_instance" "mysql" {
 
 resource "aws_launch_configuration" "launchme" {
   name          = "web_config"
-  image_id      = data.aws_ami.ubuntu.id
+  image_id      = "ami-0d563aeddd4be7fff"
   instance_type = "t2.micro"
   user_data = <<-EOF
 #!/bin/bash
@@ -49,21 +49,3 @@ resource "aws_autoscaling_group" "bar" {
   launch_configuration      = aws_launch_configuration.launchme.name
   vpc_zone_identifier = [aws_subnet.public1.id, aws_subnet.public2.id]
  }
-
-  
-  
-  data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
