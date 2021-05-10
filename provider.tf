7044b45c4e9fc22ced7b41a8f62bb178d3c2e9e7
jeffy555 <jeffersonimmanuel5@gmail.com>
2021-05-10 15:19:17 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
     region = "us-east-2"
-    access_key = ${AWS_ACCESS_KEY_ID}
-    secret_key = ${AWS_SECRET_ACCESS_KEY}
+    access_key = ${accesskey}
+    secret_key = ${secretkey}
 }
