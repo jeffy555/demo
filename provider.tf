2957569740ce8888c05d8bb078827adf5654535e
jeffy555 <jeffersonimmanuel5@gmail.com>
2021-05-10 16:19:04 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
     region = "us-east-2"
-    access_key = "AKIAXWJR6YVT2UZXIU3F"
-    secret_key = "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"
+    access_key = ${accesskey}
+    secret_key = ${secretkey}
 }
