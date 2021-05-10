4b4c2b387ba517120f201cb96974960d37dadd06
jeffy555 <jeffersonimmanuel5@gmail.com>
2021-05-10 15:39:58 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -104,7 +104,6 @@ resource "aws_db_subnet_group" "mysql" {
 resource "aws_subnet" "public1" {
   vpc_id            = aws_vpc.vpc.id
   cidr_block        = "10.0.1.0/24"
-  availability_zone = data.aws_availability_zones.azs.names[0]
 
 
 }
@@ -112,7 +111,5 @@ resource "aws_subnet" "public1" {
 resource "aws_subnet" "public2" {
   vpc_id     = aws_vpc.vpc.id
   cidr_block = "10.0.2.0/24"
-
-  availability_zone = data.aws_availability_zones.azs.names[1]
 
 }
