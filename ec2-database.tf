3a11dab3c1d351dcef507f7301e9f7aeb32f19ff
jeffy555 <jeffersonimmanuel5@gmail.com>
2021-05-10 14:47:42 +0530

    Create ec2-database.tf

---
+++ ec2-database.tf
@@ -0,0 +1,52 @@
+resource "aws_db_instance" "mysql" {
+  allocated_storage      = 20
+  storage_type           = "gp2"
+  engine                 = "mysql"
+  engine_version         = "5.7"
+  instance_class         = "db.t2.micro"
+  name                   = var.dbname
+  username               = var.username
+  password               = var.password
+  parameter_group_name   = "default.mysql5.7"
+  vpc_security_group_ids = [aws_security_group.mysql.id]
+  db_subnet_group_name   = aws_db_subnet_group.mysql.name
+  skip_final_snapshot    = true
+}
+
+
+resource "aws_instance" "ec2" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+
+  depends_on = [
+    aws_db_instance.mysql,
+  ]
+
+  key_name                    = "Session1"
+  vpc_security_group_ids      = [aws_security_group.web.id]
+  subnet_id                   = aws_subnet.public1.id
+  associate_public_ip_address = true
+
+  user_data = file("files/userdata.sh")
+
+  tags = {
+    Name = "EC2 Instance"
+  }
+  
+  
+  
+  data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
