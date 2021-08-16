a18262123b1fc0d63b0813a59f1c61c8a4846d03
jeffy555 <jeffersonimmanuel5@gmail.com>
2021-08-16 08:54:30 -0500

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,4 +1,6 @@
-resource "aws_instance" "demo" {
+module "ec2" {
+  source  = "app.terraform.io/tf-mycloud/ec2/aws"
+  version = "1.0.0"
   ami           = var.ami # ap-south-1
   instance_type = var.instance_type
   availability_zone = var.availability_zone
