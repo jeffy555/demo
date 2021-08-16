5d229dea57e32b5a8aa8acf4834d7fbbedeff992
jeffy555 <jeffersonimmanuel5@gmail.com>
2021-08-16 01:35:48 -0500

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,5 +1,5 @@
 module "website_s3_bucket" {
-  source = "./modules/aws-s3-static"  # Mandatory
+  source = "s3://terraform-modules-jefferson/aws-s3-static.zip"  # Mandatory
   bucket_name = var.my_s3_bucket
   tags = var.my_s3_tags
 }
