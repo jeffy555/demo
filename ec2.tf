80be1904025c3b2ed4f8e605cad430f3c6c4c6f0
Jefferson Immanuel R <Jefferson.Immanuel@mrcooper.com>
2021-08-16 02:43:08 -0500

    New files

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,4 @@
-module "website_ec2" {
-  source = "s3::https://terraform-modules-jefferson.s3.ap-south-1.amazonaws.com/aws-ec2-static.zip"  # Mandatory
+resource "aws_instance" "demo" {
   ami           = var.ami # ap-south-1
   instance_type = var.instance_type
   availability_zone = var.availability_zone
