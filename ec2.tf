3761f0c5de645541a1b7430cde12d92e920c4c70
jeffy555 <jeffersonimmanuel5@gmail.com>
2021-08-16 02:14:38 -0500

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 module "website_ec2" {
-  source = "s3://terraform-modules-jefferson/aws-ec2-static.zip"  # Mandatory
+  source = "s3::https://terraform-modules-jefferson.s3.ap-south-1.amazonaws.com/aws-ec2-static.zip"  # Mandatory
   ami           = var.ami # ap-south-1
   instance_type = var.instance_type
   availability_zone = var.availability_zone
